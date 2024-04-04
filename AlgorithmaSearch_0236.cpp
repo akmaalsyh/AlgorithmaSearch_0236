b5d54dd451416f1bc2fe4f53e790c7b28f294d2c
akmaalsyh <taufansyah50@gmail.com>
2024-04-04 15:09:49 +0700

    memasukkan do dan step 1

--- AlgorithmaSearch_0236.cpp
+++ AlgorithmaSearch_0236.cpp
@@ -32,3 +32,9 @@ void LinearSearch()
     char ch;
     int ctr; // Number of comparisons
     int item;
+
+    do
+    {
+        // Accept the number to be searched
+        cout << "\nEnter the element you want to search: "; // step 1
+       

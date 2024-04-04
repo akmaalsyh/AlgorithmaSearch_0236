9eb8198520504be92434dbe6b42754d471fcfe6c
akmaalsyh <taufansyah50@gmail.com>
2024-04-04 14:59:16 +0700

    memasukkan while if

--- AlgorithmaSearch_0236.cpp
+++ AlgorithmaSearch_0236.cpp
@@ -5,4 +5,11 @@ int arr[20];    // Array to be searched
 int n;          // Number of elements in the array
 int i;          // Index of array elements
 
-void input(){}
+void input()
+{
+    while (true)
+    {
+        cout << "Enter the number of elements in the array: ";
+        cin >> n;
+        if ((n > 0) && (n <= 20))
+            
